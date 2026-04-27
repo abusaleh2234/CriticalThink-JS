c3b2dfe017526ef7f70354e919e382c77ec9a8ee
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-04-27 16:00:34 +0600

    reduce explore

--- M2/reduce.js
+++ M2/reduce.js
@@ -3,3 +3,28 @@ const cartItems = [
   { id: "p-002", name: "Walton USB-C Cable", price: 350, quantity: 2 },
   { id: "p-003", name: "Aarong Kurta", price: 2200, quantity: 1 },
 ];
+
+const subTotal = cartItems.reduce((subTotal, currentItem ) => {
+    return subTotal + (currentItem.price * currentItem.quantity)}, 0)
+
+// console.log(subTotal);
+
+
+const players = [
+  { name: "Jamal Bhuyan", score: 88 },
+  { name: "Shekh Morsalin", score: 81 },
+  { name: "Rakib Hossain", score: 95 },
+  { name: "Topu Barman", score: 91 },
+  { name: "Sohel Rana", score: 72 },
+];
+
+
+const bestScorer = players.reduce((bestPlayer,player) => {
+    // console.log(bestPlayer)
+    if (bestPlayer.score > player.score) {
+        return bestPlayer
+    }
+    return player
+}, players[0])
+
+// console.log(bestScorer);
